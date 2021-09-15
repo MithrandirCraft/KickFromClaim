d170354ec3cd6d66d631384659b9b03dfac1413b
Dylan Calaf <mithrandircraft@hotmail.es>
2021-09-15 21:44:41 +0200

    Messages stuff

--- src/main/java/es/mithrandircraft/kickfromclaim/LocaleManager.java
+++ src/main/java/es/mithrandircraft/kickfromclaim/LocaleManager.java
@@ -30,11 +30,11 @@ class LocaleManager {
         localeConfiguration.addDefault("SuccessfulKick","&aEl/la jugador/a especificado ha sido expulsad@ de tu/s claim/s. Utiliza &e/setclaimflag noenterplayer <nombreDeJugador/a> &asi no quieres que vuelva a entrar.");
         localeConfiguration.addDefault("Kicked","&4Has sido expulsad@ de un claim de {Expulsor}");
 
-        localeConfiguration.addDefault("EnteredInteractKickMode","&aEntraste en modo expulsión. Haz click derecho sobre jugadores/as o mascotas que quieras expulsar de tu claim. Sal del modo expulsión utilizando &e/ck, kfc, kickfromclaim &ade nuevo");
+        localeConfiguration.addDefault("EnteredInteractKickMode","&aEntraste en modo expulsión. Haz click derecho sobre jugadores/as o mascotas que quieras expulsar de tu claim. Sal del modo expulsión utilizando &e/ck, kfc, claimkick &ade nuevo");
         localeConfiguration.addDefault("ExitedInteractKickMode","&4Saliste del modo expulsión.");
 
         localeConfiguration.addDefault("AnimalOrMobNotInYourClaim","&4Animal o mob no se encuentra en tu claim.");
-        localeConfiguration.addDefault("InvalidInteractionExpulsionEntity","&4Intentaste expulsar a una entidad invalida, solo es posible expulsar Animales o Mobs. Sal del modo expulsión utilizando &e/ck, kfc, kickfromclaim &ade nuevo");
+        localeConfiguration.addDefault("InvalidInteractionExpulsionEntity","&4Intentaste expulsar a una entidad invalida, solo es posible expulsar Animales o Mobs. Sal del modo expulsión utilizando &e/ck, kfc, claimkick &ade nuevo");
 
         localeConfiguration.addDefault("SuccessfulKickAnimalOrMob","&aAnimal o mob expulsados de tu claim.");
     }
